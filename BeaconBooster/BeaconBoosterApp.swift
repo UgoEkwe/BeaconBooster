2bf92190e49571ed7e240548fc893026f7296d05
Ugonna Chaekwe <39320450+UgoEkwe@users.noreply.github.com>
2024-12-10 01:31:34 -0600

    Initial Commit

---
+++ BeaconBooster/BeaconBoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconBoosterApp.swift
+//  BeaconBooster
+//
+//  Created by Ugonna Oparaochaekwe on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconBoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
